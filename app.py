beebfedc32fe7d093a718f8ba8828ffbdf93b25c
devesh <devesh@bytesview.com>
2025-11-21 12:06:52 +0530

    first flask application

---
+++ app.py
@@ -0,0 +1,28 @@
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return "Hello Friends! it's Day 1"
+
+@app.route("/contact")
+def contact():
+	con_details = {'Name':'Devesh Yadav','Phone No.':'+917060693600','Email':'deveshyadav7060@gmail.com'}
+	return con_details
+
+@app.route("/about")
+def aboute():
+	return  "Hello, I'm Python Developer, Experties in Web Scraping!"
+
+@app.route("/hello")
+def hello():
+	name = request.args.get('name','Guest')
+	return jsonify(status = "oK",message = f"Hello {name.title()} Welcome to Flask Development!")
+	
+
+if __name__ == "__main__":
+	app.run(debug=True)
